7c494b50d5be325e23a139f4245dd4a2e80d0ee6
AsdhyM <asdhyme@gmail.com>
2024-09-13 15:30:52 +1000

    feat: bootstrap component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 
 export default function Header() {
     return(
-        <h1>React props lesson</h1>
+        <div className="App-header">
+            <h1>React props lesson</h1>
+        </div>
     )
 }
 // module.exports = Header and skip the export default
